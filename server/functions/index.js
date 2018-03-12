53f3e6b813ea40e907e5b4ffc4dfb4a434e8f409
camiloriosf <camilo.rios.f@gmail.com>
2018-03-11 21:29:01 -0300

    first commit

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,11 +1,16 @@
 const functions = require('firebase-functions');
 const cors = require('cors');
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.use(cors({ origin: true }));
 app.get('*', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ oops: 'invalid endpoint' });
 });
 
 const api = functions.https.onRequest((req, res) => {
