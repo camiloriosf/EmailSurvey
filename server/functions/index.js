ea60480a97f7e6b3fe6f5bf967b7ff9f7e573a33
camiloriosf <camilo.rios.f@gmail.com>
2018-03-11 22:30:36 -0300

    create routes to handle google auth

--- server/functions/index.js
+++ server/functions/index.js
@@ -5,13 +5,35 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
+const keys = functions.config().auth.google.dev;
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.id,
+      clientSecret: keys.secret,
+      callbackURL: '/api/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.use(cors({ origin: true }));
-app.get('*', (req, res) => {
-  res.send({ oops: 'invalid endpoint' });
-});
+
+app
+  .get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
+  .get('/auth/google/callback', (req, res) => {
+    res.send({
+      data: req.query
+    });
+  });
 
 const api = functions.https.onRequest((req, res) => {
   if (!req.path) req.url = `/${req.url}`;
